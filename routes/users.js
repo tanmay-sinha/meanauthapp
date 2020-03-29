6d67c891b8a1153f1c98fa5f5d094f38cb6907b3
Tanmay Sinha <alokratn001@gmail.com>
2020-03-29 18:21:31 +0530

    added basic registration in database

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router();
+const User = require('../models/user')
 
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user)=>{
+        if(err){
+            res.json({success: false, msg:'Failed to register new user'});
+        } else {
+            res.json({success:true, msg:'successfully registered'});
+        }
+    });
 });
 
 router.post('/authenticate', (req, res, next) => {
